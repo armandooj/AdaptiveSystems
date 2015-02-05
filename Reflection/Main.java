7f8aa93e996e4558322b328cb9c606ab2c2858ca
Armando Ochoa Jimenez <ochoaa@ensipc102.imag.fr>
2015-02-05 09:18:34 +0100

    Add a simple reflection program

---
+++ Reflection/Main.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Method;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Taco tacoPastor = new Taco("al pastor");
+		System.out.println("Quiero un taco " + tacoPastor.getName());
+		
+		// How to get the class, if the instance is available
+		System.out.println(tacoPastor.getClass());
+		
+		for (int i = 0; i < Double.class.getMethods().length; i++) {
+			System.out.println(Double.class.getMethods()[i]);
+		}
+		
+		// Invoke a method using the Method object
+		Method method;
+		try {
+			method = tacoPastor.getClass().getMethod("getName", (Class<?> []) null);
+			method.invoke(tacoPastor, (Object[]) null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+}
---
+++ Reflection/Taco.java
@@ -0,0 +1,19 @@
+
+public class Taco {
+
+	private String name;
+	
+	public Taco(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		System.out.println("hey");
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
